2ad502cff8c75746b643f2e588a93177498e5343
Aseem Pradhan <2k22.cse.2213048@gmail.com>
2025-09-06 20:24:49 +0530

    tree started

---
+++ Advance DSA/Tree/T_implementation.java
@@ -0,0 +1,10 @@
+public class T_implementation {
+    public static void main(String[] args) {
+        Node root = new Node(5);
+        root.left = new Node(2);
+        root.right = new Node(8);
+        System.out.println(root.data);
+        System.out.println(root.right.data);
+        System.out.println(root.left.data);
+    }
+}
